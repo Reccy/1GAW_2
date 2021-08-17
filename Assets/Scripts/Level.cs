dc2b2685888fca6e242b63222d8784ce1f466c14
Aaron Meaney <aaronmeaney@hotmail.com>
2021-08-17 17:18:02 +0100

    progress

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -11,30 +11,109 @@ public class Level : MonoBehaviour
 
     private Tile[] m_tiles;
 
+    private void Awake()
+    {
+        LoadFromHierarchy();
+    }
+
+    private void Update()
+    {
+        Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        pos = transform.InverseTransformPoint(pos);
+
+        Vector2Int p = new Vector2Int(Mathf.RoundToInt(pos.x), Mathf.RoundToInt(pos.y));
+
+        Tile t = GetTileAt(p.x, p.y);
+
+        Debug.Log($"{GetPosition(t)} - N:({GetPosition(t.NorthNeighbour())}, S:({GetPosition(t.SouthNeighbour())}), E:({GetPosition(t.EastNeighbour())}), W:({GetPosition(t.WestNeighbour())})");
+
+        DebugDraw.DrawCross(pos, Color.red);
+        DebugDraw.DrawCross(p, Color.blue);
+
+        if (t == null)
+            return;
+
+        if (t.NorthNeighbour().Pass == Tile.PassMode.PASSABLE)
+        {
+            DebugDraw.DrawArrow(t.transform.position, t.NorthNeighbour().transform.position, Color.green);
+        }
+        else
+        {
+            DebugDraw.DrawArrow(t.transform.position, t.NorthNeighbour().transform.position, Color.red);
+        }
+
+        if (t.SouthNeighbour().Pass == Tile.PassMode.PASSABLE)
+        {
+            DebugDraw.DrawArrow(t.transform.position, t.SouthNeighbour().transform.position, Color.green);
+        }
+        else
+        {
+            DebugDraw.DrawArrow(t.transform.position, t.SouthNeighbour().transform.position, Color.red);
+        }
+
+        if (t.EastNeighbour().Pass == Tile.PassMode.PASSABLE)
+        {
+            DebugDraw.DrawArrow(t.transform.position, t.EastNeighbour().transform.position, Color.green);
+        }
+        else
+        {
+            DebugDraw.DrawArrow(t.transform.position, t.EastNeighbour().transform.position, Color.red);
+        }
+
+        if (t.WestNeighbour().Pass == Tile.PassMode.PASSABLE)
+        {
+            DebugDraw.DrawArrow(t.transform.position, t.WestNeighbour().transform.position, Color.green);
+        }
+        else
+        {
+            DebugDraw.DrawArrow(t.transform.position, t.WestNeighbour().transform.position, Color.red);
+        }
+    }
+
     public void LoadFromHierarchy()
     {
         m_tiles = GetComponentsInChildren<Tile>();
+
+        foreach (Tile tile in m_tiles)
+        {
+            tile.SubscribeToLevel(this);
+        }
+
         Debug.Log($"Loaded {m_tiles.Length} tiles");
     }
 
-    public void Validate()
+#if UNITY_EDITOR
+    public bool Validate()
     {
         LoadFromHierarchy();
+
+        bool valid = true;
 
         foreach (Tile tile in m_tiles)
         {
             Vector2Int position = GetPosition(tile);
 
-            if (tile.transform.position.x != position.x && tile.transform.position.y != position.y)
+            if (tile.transform.position.x != position.x || tile.transform.position.y != position.y)
+            {
                 Debug.LogWarning($"Tile {tile.name} has real position {tile.transform.position}, but index of {position}", tile);
+                valid = false;
+            }
         }
 
-        Debug.Log("Validation complete");
+        if (!valid)
+        {
+            Debug.LogWarning("Validation complete with errors");
+        }
+        else
+        {
+            Debug.Log("Validation complete with no errors");
+        }
 
-        m_tiles = null;
+        return valid;
     }
+#endif
 
-    private Vector2Int GetPosition(Tile tile)
+    public Vector2Int GetPosition(Tile tile)
     {
         for (int x = 0; x < m_width; ++x)
         {
@@ -50,5 +129,16 @@ public class Level : MonoBehaviour
         }
 
         return new Vector2Int(-1, -1);
+    }
+
+    public Tile GetTileAt(int x, int y)
+    {
+        x = Mathf.Abs(x % m_width);
+        y = Mathf.Abs(y % m_height);
+        int i = x + y * m_width;
+
+        Tile t = m_tiles[i];
+
+        return t;
     }
 }
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -3,5 +3,44 @@ using UnityEngine;
 [SelectionBase]
 public class Tile : MonoBehaviour
 {
+    public enum PassMode
+    {
+        IMPASSABLE,
+        ENEMY_ONLY,
+        PASSABLE
+    }
 
+    private Level m_level;
+    private Vector2Int m_pos;
+
+    [SerializeField]
+    private PassMode m_passMode;
+
+    public PassMode Pass { get { return m_passMode; } }
+
+    public void SubscribeToLevel(Level level)
+    {
+        m_level = level;
+        m_pos = m_level.GetPosition(this);
+    }
+
+    public Tile NorthNeighbour()
+    {
+        return m_level.GetTileAt(m_pos.x, m_pos.y + 1);
+    }
+
+    public Tile SouthNeighbour()
+    {
+        return m_level.GetTileAt(m_pos.x, m_pos.y - 1);
+    }
+
+    public Tile EastNeighbour()
+    {
+        return m_level.GetTileAt(m_pos.x + 1, m_pos.y);
+    }
+
+    public Tile WestNeighbour()
+    {
+        return m_level.GetTileAt(m_pos.x - 1, m_pos.y);
+    }
 }
